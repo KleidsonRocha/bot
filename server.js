1fc7052bc347fe3c2a5e9e935436ae86fa2e4ca4
KleidsonRocha <73981568+KleidsonRocha@users.noreply.github.com>
2020-11-05 03:14:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 const Tweet = new Twitter({
   consumer_key:         process.env.BOT_CONSUMER_KEY,
   consumer_secret:      process.env.BOT_CONSUMER_SECRET,
-  access_token_key:     process.env.BOT_ACESS_TOKEN,
-  access_token_secret:  process.env.BOT_ACESS_TOKEN_SECRET,
+  access_token_key:     process.env.BOT_ACCESS_TOKEN,
+  access_token_secret:  process.env.BOT_ACCESS_TOKEN_SECRET,
 })
 
 function action(event){
